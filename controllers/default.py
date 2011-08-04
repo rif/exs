80368168f2236da74dceb9091f441f617e0f6e3e
Radu Ioan Fericean <rif@love.ro>
2011-08-04 17:46:54 +0300

    tooltips

--- controllers/default.py
+++ controllers/default.py
@@ -23,10 +23,19 @@ def proiecte():
     if not proiecte:
         proiecte = db(db.project)
     proiecte = proiecte.select(orderby=~db.project.year, limitby=limitby)
+    years = [] 
+    [years.append(p.year) for p in proiecte if not p.year in years] 
     
     ids = ('doi-doi', 'doi-trei', 'doi-patru', 'trei-doi', 'trei-trei', 'trei-patru')
-    divs = [DIV(IMG(_src=URL('download', args=db((db.picture.project==p)&(db.picture.representative==True)).select().first().image), _alt=p.name +'picture', _title=p.year), _id=ids[i%items_per_page]) for i,p in enumerate(proiecte)]
-    return dict(divs=divs, page=page,items_per_page=items_per_page)
+    divs = []
+    for i,p in enumerate(proiecte):
+        pic = db((db.picture.project==p)&(db.picture.representative==True)).select().first()
+        if not pic: pic = db(db.picture.project==p).select().first() # get the first image
+        if pic:
+            divs.append(DIV(IMG(_src=URL('download', args=pic.image), _alt=p.name +' picture', _title=str(p.year) + ": " + p.description, _class="prj-img"), _id=ids[i%items_per_page]))
+        else: # if project has no pictures
+            divs.append(DIV(P('No picture for project %s' % p.name), _id=ids[i%items_per_page]))
+    return dict(divs=divs, page=page,items_per_page=items_per_page, years=years)
 
 def contact():
     form = SQLFORM.factory(
