89205205d94b70bb2964a502d06a13cc1add9067
Radu Ioan Fericean <radu@fericean.ro>
2011-08-04 16:06:38 +0300

    galeria de proiecte fara ani

--- controllers/default.py
+++ controllers/default.py
@@ -7,7 +7,26 @@ def despre_noi():
     return locals()
 
 def proiecte():
-    return locals()
+    categoria = db.category(request.vars['cat']) if 'cat' in request.vars else  None
+    tagul = db.tag(request.vars['tag']) if 'tag' in request.vars else  None
+    
+    if len(request.args): page=int(a0)
+    else: page=0
+    items_per_page=6
+    limitby=(page*items_per_page,(page+1)*items_per_page+1)
+    
+    proiecte = None
+    if categoria:
+        proiecte = categoria.project
+    if categoria and tagul:
+        proiecte = db((db.project.category==categoria) & (db.project.tags.contains(tagul.id)))
+    if not proiecte:
+        proiecte = db(db.project)
+    proiecte = proiecte.select(orderby=~db.project.year, limitby=limitby)
+    
+    ids = ('doi-doi', 'doi-trei', 'doi-patru', 'trei-doi', 'trei-trei', 'trei-patru')
+    divs = [DIV(IMG(_src=URL('download', args=db((db.picture.project==p)&(db.picture.representative==True)).select().first().image), _alt=p.name +'picture', _title=p.year), _id=ids[i%items_per_page]) for i,p in enumerate(proiecte)]
+    return dict(divs=divs, page=page,items_per_page=items_per_page)
 
 def contact():
     form = SQLFORM.factory(
