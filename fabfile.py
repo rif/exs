b32224088a4a3b6ef2c30c005a51cc2b6f5c6753
Radu Ioan Fericean <radu@fericean.ro>
2011-08-04 12:39:06 +0300

    green prints

--- fabfile.py
+++ fabfile.py
@@ -4,19 +4,21 @@ from fabric.colors import green
 
 @task
 def ci():
-    """Commit localy using mercurial"""
+    'Commit localy using mercurial'
     comment = prompt('Commit comment: ', default='another commit from fabric')
     local('hg ci -m "%s"' % comment)
     push()
 
 @runs_once
 def push():
+    print(green('pushing...'))
     local('hg push')
 
 @task
 @hosts('rif@avocadosoft.ro:22011')
 def deploy():
     'triggers hg pul on the server'
+    print(green('deploying...'))
     push()
     'Deploy the app to the target environment'
     with cd('../www-data/web2py/applications/exs/'):
@@ -26,4 +28,5 @@ def deploy():
 @hosts('rif@avocadosoft.ro:22011')
 def reload():
     'fires an apache graceful reload'
+    print(green('reloading...'))
     sudo('apachectl graceful')
