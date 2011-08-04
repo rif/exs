af87fe5f0bb23eb84bfa0ef1cc8e035fc506458a
Radu Ioan Fericean <radu@fericean.ro>
2011-08-04 12:35:26 +0300

    better fabric

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,26 @@
-from fabric.api import hosts, prompt, sudo, run, local, cd
+from fabric.api import sudo, prompt, run, local, cd
+from fabric.decorators import runs_once, hosts, task
 
+@task
 def ci():
     """Commit localy using mercurial"""
     comment = prompt('Commit comment: ', default='another commit from fabric')
     local('hg ci -m "%s"' % comment)
+    push()
+
+@runs_once
+def push():
     local('hg push')
 
+@task
 @hosts('rif@avocadosoft.ro:22011')
 def deploy():
+    push()
     'Deploy the app to the target environment'
-    #local('hg push')
     with cd('../www-data/web2py/applications/exs/'):
         run('hg pul -uv')
 
+@task
 @hosts('rif@avocadosoft.ro:22011')
 def reload():
     'fires an apache graceful reload'
