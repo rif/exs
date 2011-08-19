9a8a0f3f960e8a5e2e3cdea2ad2f272d7fadfb8d
Radu Ioan Fericean <radu@fericean.ro>
2011-08-19 12:34:39 +0300

    removed about us

--- models/menu.py
+++ models/menu.py
@@ -17,7 +17,6 @@ response.meta.generator = 'Web2py Enterprise Framework'
 
 response.menu = [
     (T('HOME'), False, URL('default','index'), []),
-    (T('DESPRE NOI'), False, URL('default','despre_noi'), []),
     (T('PROIECTE'), False, URL('default','proiecte'), []),
     (T('CONTACT'), False, URL('default','contact'), [])
     ]
